0b9d22a994c2a3ea523f10f39a6db1490f0a5d83
Bruno Bastos <brunomauro@gmail.com>
2023-09-07 21:02:52 -0300

    refactor rotas de location

--- app/services/location_service.py
+++ app/services/location_service.py
@@ -1,14 +1,33 @@
 # /api-bridge/app/services/location_service.py
 import requests
+from flask_restx import abort
+from app.constants import CITY_SEARCH_CACHE_TIMEOUT
+from app import cache
+from ..config import *
 
 
-def get_location_data(cep):
+def search_city(city_name):
+    cache_key = f"city_search_{city_name}"
+
     try:
-        url = f"https://viacep.com.br/ws/{cep}/json/"
+        if cache.get(cache_key):
+            return cache.get(cache_key)
+        # https://api.openweathermap.org/geo/1.0/direct?q=janeiro&appid=0f407f8dd956c7204bc02a9c3423c83c&lang=pt_br&limit=5
+        url = f"{GEOCODE_API_URL}/direct?q={city_name}&{GEOCODE_API_DEFAULT_QUERYSTRING}"
         response = requests.get(url)
+        if response.status_code != 200:
+            return {"error": "Erro ao buscar dados da API do clima."}
+
         data = response.json()
-        if "erro" in data:
-            return {"error": "CEP não encontrado"}
-        return data
-    except Exception as e:
-        return {"error": "Erro desconhecido"}
+        cities = [{
+            "name": item["name"],
+            "country": item["country"],
+            "state": item["state"] if "state" in item else "",
+            "lat": item["lat"],
+            "lon": item["lon"]
+        } for item in data]
+
+        cache.set(cache_key, cities, timeout=CITY_SEARCH_CACHE_TIMEOUT)
+        return cities
+    except requests.RequestException:
+        abort(500, "Erro ao buscar dados da API do clima.")
